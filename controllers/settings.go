b1f90cc6b2467ccb475d6960fa600ad8069ace84
matancarmeli7 <matan.carmeli7@gmail.com>
2023-01-01 11:02:09 +0200

    add events
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- controllers/settings.go
+++ controllers/settings.go
@@ -1,7 +1,13 @@
 package controllers
 
 var (
-	addingPVC   = "addPVC"
-	removingPVC = "removePVC"
-	vgReconcile = "vgReconcile"
+	addingPVC      = "addPVC"
+	removingPVC    = "removePVC"
+	vgReconcile    = "vgReconcile"
+	deleteVG       = "deletingVG"
+	createVG       = "creatingVG"
+	createVGC      = "creatingVGC"
+	updateVGC      = "updatingVGC"
+	updateStatusVG = "updatingStatusVG"
+	updateStatusVGC = "updatingStatusVGC"
 )
