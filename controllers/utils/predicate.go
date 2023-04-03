153a88c980a96d561ff136d6b0663adbd5accecd
ArbelNathan <arbel.nathan@ibm.com>
2023-04-03 14:04:18 +0300

    clean
    Signed-off-by: ArbelNathan <arbel.nathan@ibm.com>

--- controllers/utils/predicate.go
+++ controllers/utils/predicate.go
@@ -7,7 +7,7 @@ import (
 	volumegroupv1 "github.com/IBM/csi-volume-group-operator/api/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -22,25 +22,14 @@ func FinalizerPredicate() predicate.Predicate {
 	}
 }
 
-func CreateRequests(kclient client.Client) handler.EventHandler {
+func CreateRequests(client runtimeclient.Client) handler.EventHandler {
 	return handler.EnqueueRequestsFromMapFunc(
-		func(object client.Object) []reconcile.Request {
-			// Get all VolumeGroup objects with a label selector that matches the labels of the PersistentVolumeClaim.
-			//selector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
-			//	MatchLabels: object.GetLabels(),
-			//})
-			//if err != nil {
-			//	return []ctrl.Request{}
-			//}
-			//listOptions := []client.ListOption{
-			//	client.MatchingLabelsSelector{Selector: selector},
-			//}
-
+		func(object runtimeclient.Object) []reconcile.Request {
 			var vgList volumegroupv1.VolumeGroupList
-			if err := kclient.List(context.TODO(), &vgList); err != nil {
+			if err := client.List(context.TODO(), &vgList); err != nil {
 				return []ctrl.Request{}
 			}
-
+			// TODO  CSI-5437 - add a label selector check to the VolumeGroup to filter the list
 			// Create a reconcile request for each matching VolumeGroup.
 			requests := make([]ctrl.Request, len(vgList.Items))
 			for _, vg := range vgList.Items {
