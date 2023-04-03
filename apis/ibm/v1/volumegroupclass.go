ce9be693ee81dd9d5aa0be4e34d24a53bf31121a
matancarmeli7 <matan.carmeli7@gmail.com>
2023-04-03 09:49:30 +0300

    fix GetDeletionPolicy in vgclass
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- apis/ibm/v1/volumegroupclass.go
+++ apis/ibm/v1/volumegroupclass.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,5 +30,5 @@ func (vgc *VolumeGroupClass) GetDeletionPolicy() common.VolumeGroupDeletionPolic
 	if deletionPolicy == string(common.VolumeGroupContentDelete) {
 		return common.VolumeGroupContentDelete
 	}
-	return common.VolumeGroupContentDelete
+	return common.VolumeGroupContentRetain
 }
