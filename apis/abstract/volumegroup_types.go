21252b5df0e768477fb19fa42fbd86b12c6d0b5a
matancarmeli7 <matan.carmeli7@gmail.com>
2023-04-18 10:38:50 +0300

    add abstract functions
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

---
+++ apis/abstract/volumegroup_types.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package abstract
+
+import (
+	"github.com/IBM/csi-volume-group-operator/apis/common"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type VolumeGroup interface {
+	metav1.Object
+	runtime.Object
+	GetVGCName() string
+	GetVGCLassName() string
+	GetApiVersion() string
+	GetSelector() *metav1.LabelSelector
+	GetPVCList() []corev1.PersistentVolumeClaim
+	IsReady() bool
+	UpdateVGCName(vgcName string)
+	UpdateBoundVGCName(vgcName string)
+	UpdateGroupCreationTime(groupCreationTime *metav1.Time)
+	UpdateReady(ready bool)
+	UpdateError(vgError *common.VolumeGroupError)
+	UpdatePVCList(PVCList []corev1.PersistentVolumeClaim)
+	Copy() VolumeGroup
+}
+
+type VolumeGroupList interface {
+	GetItems() []VolumeGroup
+	metav1.ListInterface
+	runtime.Object
+}
---
+++ apis/ibm/v1/volumegroupcontent.go
@@ -0,0 +1,122 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package v1
+
+import (
+	"reflect"
+
+	"github.com/IBM/csi-volume-group-operator/apis/abstract"
+	"github.com/IBM/csi-volume-group-operator/apis/common"
+	"github.com/IBM/csi-volume-group-operator/pkg/utils"
+	pkg_utils "github.com/IBM/csi-volume-group-operator/pkg/utils"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (vgc *VolumeGroupContent) GetVGCLassName() string {
+	return utils.GetStringField(vgc.Spec, "VolumeGroupClassName")
+}
+func (vgc *VolumeGroupContent) GetVGHandle() string       { return vgc.Spec.Source.VolumeGroupHandle }
+func (vgc *VolumeGroupContent) GetVGRefName() string      { return vgc.Spec.VolumeGroupRef.Name }
+func (vgc *VolumeGroupContent) GetVGRefNamespace() string { return vgc.Spec.VolumeGroupRef.Namespace }
+func (vgc *VolumeGroupContent) GetSource() reflect.Value {
+	return utils.GetObjectField(vgc.Spec, "Source")
+}
+func (vgc *VolumeGroupContent) GetVGRef() reflect.Value {
+	return utils.GetObjectField(vgc.Spec, "VolumeGroupRef")
+}
+func (vgc *VolumeGroupContent) GetDeletionPolicy() common.VolumeGroupDeletionPolicy {
+	deletionPolicy := utils.GetStringField(vgc.Spec, "VolumeGroupDeletionPolicy")
+	if deletionPolicy == "" {
+		return ""
+	}
+	if deletionPolicy == string(common.VolumeGroupContentDelete) {
+		return common.VolumeGroupContentDelete
+	}
+	return common.VolumeGroupContentRetain
+}
+func (vgc *VolumeGroupContent) GetVGSecretRef() *corev1.SecretReference {
+	return vgc.Spec.VolumeGroupSecretRef
+}
+func (vgc *VolumeGroupContent) GetPVList() []corev1.PersistentVolume { return vgc.Status.PVList }
+func (vgc *VolumeGroupContent) IsReady() bool {
+	return utils.GetBoolField(vgc.Status, "Ready")
+}
+func (vgc *VolumeGroupContent) UpdateVGRef(vgRef *corev1.ObjectReference) {
+	vgc.Spec.VolumeGroupRef = vgRef
+}
+func (vgc *VolumeGroupContent) UpdateVGClassName(vgclassName string) {
+	vgc.Spec.VolumeGroupClassName = &vgclassName
+}
+func (vgc *VolumeGroupContent) UpdateSecretRef(secretRef *corev1.SecretReference) {
+	vgc.Spec.VolumeGroupSecretRef = secretRef
+}
+func (vgc *VolumeGroupContent) UpdateDeletionPolicy(deletionPolicy *common.VolumeGroupDeletionPolicy) {
+	vgc.Spec.VolumeGroupDeletionPolicy = deletionPolicy
+}
+func (vgc *VolumeGroupContent) UpdateVGHandle(vgHandle string) {
+	vgc.Spec.Source.VolumeGroupHandle = vgHandle
+}
+func (vgc *VolumeGroupContent) UpdateVGAttributes(vgAttributes map[string]string) {
+	vgc.Spec.Source.VolumeGroupAttributes = vgAttributes
+}
+func (vgc *VolumeGroupContent) UpdatePVList(PVList []corev1.PersistentVolume) {
+	vgc.Status.PVList = PVList
+}
+func (vgc *VolumeGroupContent) UpdateError(vgError *common.VolumeGroupError) {
+	vgc.Status.Error = vgError
+}
+func (vgc *VolumeGroupContent) UpdateGroupCreationTime(groupCreationTime *metav1.Time) {
+	vgc.Status.GroupCreationTime = groupCreationTime
+}
+func (vgc *VolumeGroupContent) UpdateReady(ready bool) {
+	vgc.Status.Ready = &ready
+}
+func (vgc *VolumeGroupContent) UpdateAPIVersion(apiVersion string) {
+	vgc.APIVersion = apiVersion
+}
+func (vgc *VolumeGroupContent) UpdateKind(kind string) {
+	vgc.Kind = kind
+}
+
+func (vgc *VolumeGroupContent) GenerateVGC(vgName string, vg abstract.VolumeGroup, vgClass abstract.VolumeGroupClass,
+	secretName, secretNamespace string) {
+	vgc.ObjectMeta = metav1.ObjectMeta{
+		Name:      vgName,
+		Namespace: vg.GetNamespace(),
+	}
+	vgc.Spec = generateVGCSpec(vg, vgClass, secretName, secretNamespace)
+}
+
+func generateVGCSpec(vg abstract.VolumeGroup, vgClass abstract.VolumeGroupClass,
+	secretName, secretNamespace string) VolumeGroupContentSpec {
+	vgClassName := vg.GetVGCLassName()
+	supportVolumeGroupSnapshot := false
+	return VolumeGroupContentSpec{
+		VolumeGroupClassName:       &vgClassName,
+		VolumeGroupRef:             pkg_utils.GenerateObjectReference(vg),
+		Source:                     generateVGCSource(vgClass.GetDriver()),
+		VolumeGroupDeletionPolicy:  pkg_utils.GetVolumeGroupDeletionPolicy(vgClass),
+		SupportVolumeGroupSnapshot: &supportVolumeGroupSnapshot,
+		VolumeGroupSecretRef:       pkg_utils.GenerateSecretReference(secretName, secretNamespace),
+	}
+}
+
+func generateVGCSource(driver string) *VolumeGroupContentSource {
+	return &VolumeGroupContentSource{
+		Driver: driver,
+	}
+}
